d8635395dd986c0bd8ce541bc05afacb9647bf4e
Son Vu <sonvn@smartosc.com>
2021-05-08 23:32:59 +0700

    WIP

--- p2p-router/ip-location-mapping/main.py
+++ p2p-router/ip-location-mapping/main.py
@@ -1,6 +1,6 @@
 import geoip2.database
 
-ip = "104.16.37.47"
+ip = "113.22.82.9"
 with geoip2.database.Reader('./database/GeoLite2-ASN_20210504/GeoLite2-ASN.mmdb') as reader:
     asn = reader.asn(ip)
     reader.close()
@@ -10,4 +10,4 @@ with geoip2.database.Reader('./database/GeoLite2-City_20210427/GeoLite2-City.mmd
 with geoip2.database.Reader('./database/GeoLite2-Country_20210427/GeoLite2-Country.mmdb') as reader:
     country = reader.country(ip)
     reader.close()
-print("Network: {}\t ASN: {}\t City: {}\t Country: {}".format(asn.network, asn.autonomous_system_number, city.city.name, country.country.iso_code))
+print("Network: {}\t ASN: {}\t Org: {}\t City: {}\t Country: {}".format(asn.network, asn.autonomous_system_number, asn.autonomous_system_organization, city.city.name, country.country.iso_code))
