616b0ece3d9453fd910bf669542a28940f9bc2e2
vungocson998 <vungocson998@gmail.com>
2021-05-16 21:36:37 +0700

    Use cobra init for compute module

---
+++ p2p-router/packet-capture/compute/cmd/root.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "compute",
+	Short: "compute module will calculate the bandwidth limit for each p2p peers",
+	Long: `Compute module takes the inputs from SQLite DB and then compute based on a formula.
+
+Result will be stored in bandwidth table`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.compute.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".compute" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".compute")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		_, err = fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		if err != nil {
+			fmt.Println("Error while init root Cmd Config", err.Error())
+		}
+	}
+}
---
+++ p2p-router/packet-capture/compute/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/vungocson998/XDP-tutor/p2p-router/packet-capture/compute/cmd"
+
+func main() {
+	cmd.Execute()
+}
